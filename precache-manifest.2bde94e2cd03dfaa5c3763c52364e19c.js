8a156b2970ae1f743db0bf43646f1f5f1062782d
KatDenisenko <kat.denisenko@gmail.com>
2019-01-18 10:11:26 +0200

    Updates

---
+++ precache-manifest.2bde94e2cd03dfaa5c3763c52364e19c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "912d96ff51cb971e14423813f19b4919",
+    "url": "/weather_app/static/media/magnolia-trees.912d96ff.jpg"
+  },
+  {
+    "revision": "ad811c71bce292937464",
+    "url": "/weather_app/static/css/main.11d2a0c5.chunk.css"
+  },
+  {
+    "revision": "9178a61a5ac0eb4c35833f86581161dd",
+    "url": "/weather_app/static/media/tree.9178a61a.jpg"
+  },
+  {
+    "revision": "24efd40d8ee38ad4a7b3",
+    "url": "/weather_app/static/js/1.24efd40d.chunk.js"
+  },
+  {
+    "revision": "2b8e50d38204ea6f6971",
+    "url": "/weather_app/static/js/runtime~main.2b8e50d3.js"
+  },
+  {
+    "revision": "be603912d287658e784c689d9882d01f",
+    "url": "/weather_app/static/media/balloon.be603912.jpg"
+  },
+  {
+    "revision": "30c44849fa759529ef7a2b6c324f2ad1",
+    "url": "/weather_app/static/media/bled.30c44849.jpg"
+  },
+  {
+    "revision": "5571864bf69d8947e8c946c29fd45364",
+    "url": "/weather_app/static/media/canyon.5571864b.jpg"
+  },
+  {
+    "revision": "53b0e73d1c52f9e739d14f2093e2f1bb",
+    "url": "/weather_app/static/media/countryside.53b0e73d.jpg"
+  },
+  {
+    "revision": "e5e738b3c56e5467eb917f07b5896d73",
+    "url": "/weather_app/static/media/dandelion.e5e738b3.jpg"
+  },
+  {
+    "revision": "42398b438de320f546d59d70e31b54a4",
+    "url": "/weather_app/static/media/dandelion2.42398b43.jpg"
+  },
+  {
+    "revision": "c6f949984d34f762af23565ae25ab03a",
+    "url": "/weather_app/static/media/lone-tree.c6f94998.jpg"
+  },
+  {
+    "revision": "ad811c71bce292937464",
+    "url": "/weather_app/static/js/main.ad811c71.chunk.js"
+  },
+  {
+    "revision": "0ad5cda08456c0c1d98df347ffcfbd3d",
+    "url": "/weather_app/static/media/mountain.0ad5cda0.jpg"
+  },
+  {
+    "revision": "5fd56d925a16fa07c6b416b41284db95",
+    "url": "/weather_app/static/media/ocean.5fd56d92.jpg"
+  },
+  {
+    "revision": "25524aefbe717866b21715f014442c4e",
+    "url": "/weather_app/static/media/panorama.25524aef.jpg"
+  },
+  {
+    "revision": "126e5532348eac8ab0c52d2342608863",
+    "url": "/weather_app/static/media/road.126e5532.jpg"
+  },
+  {
+    "revision": "f7e81b3d26dd2055d4298504ea46abd5",
+    "url": "/weather_app/static/media/sea.f7e81b3d.jpg"
+  },
+  {
+    "revision": "77e33cd484a08ccfa0d0235f78e96848",
+    "url": "/weather_app/static/media/second_tree.77e33cd4.jpg"
+  },
+  {
+    "revision": "5ad95ac53a16be65cdc5e0b8e5bca108",
+    "url": "/weather_app/static/media/spring.5ad95ac5.jpg"
+  },
+  {
+    "revision": "c9aa71dab35f5f8edb25ef2c0733ccae",
+    "url": "/weather_app/static/media/sunset.c9aa71da.jpg"
+  },
+  {
+    "revision": "4d2ccc43560b9d53fb93ed65b29d77c5",
+    "url": "/weather_app/static/media/the-mouth.4d2ccc43.jpg"
+  },
+  {
+    "revision": "24efd40d8ee38ad4a7b3",
+    "url": "/weather_app/static/css/1.5d18fd2e.chunk.css"
+  },
+  {
+    "revision": "37f82bbdec29e877b2ac7a041b8aaa4c",
+    "url": "/weather_app/index.html"
+  }
+];
